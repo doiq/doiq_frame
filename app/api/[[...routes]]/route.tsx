b5769545c6def3c060e179981df1b9d7f0eb1fc4
doiq <whatthedoiq@proton.me>
2024-06-27 02:12:35 +1000

    Initial commit

---
+++ app/api/[[...routes]]/route.tsx
@@ -0,0 +1,177 @@
+/** @jsxImportSource frog/jsx */
+
+import { Button, Frog, TextInput } from 'frog'
+import { devtools } from 'frog/dev'
+// import { neynar } from 'frog/hubs'
+import { handle } from 'frog/next'
+import { serveStatic } from 'frog/serve-static'
+
+const app = new Frog({
+  assetsPath: '/',
+  basePath: '/api',
+  // Supply a Hub to enable frame verification.
+  // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
+})
+
+// Uncomment to use Edge Runtime
+// export const runtime = 'edge'
+
+app.frame('/', (c) => {
+  const { buttonValue, status } = c
+  const fruit = buttonValue
+  return c.res({
+    action: '/doiq',
+    image: (
+      <div
+        style={{
+          alignItems: 'center',
+          background:'white',
+          backgroundSize: '100% 100%',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            color: 'black',
+            fontSize: 180,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.0,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          doiq?
+        </div>
+        <div
+          style={{
+            color: 'black',
+            fontSize: 60,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            padding: '0 240px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          click to doiq
+        </div>
+      </div>
+    ),
+    intents: [
+      <Button>Start</Button>,
+      <Button.Link href='https://docs.doiq.xyz/'>Leaderboard</Button.Link>,
+      <Button.Link href='https://docs.doiq.xyz/'>Learn</Button.Link>,
+    ],
+  })
+})
+
+app.frame('/doiq', (c) => {
+  const { buttonValue, status } = c
+  const fruit = buttonValue
+  return c.res({
+    action: '/result',
+    image: (
+      <div
+        style={{
+          alignItems: 'center',
+          background: 'white',
+          backgroundSize: '100% 100%',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            color: 'black',
+            fontSize: 90,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          I like to _________
+        </div>
+      </div>
+    ),
+    intents: [
+      <Button value="A">doiq</Button>,
+      <Button value="B">doiq?</Button>,
+      <Button value="C">doiq!</Button>,
+    ],
+  })
+})
+
+app.frame('/result', (c) => {
+  const { buttonValue, status } = c
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: 'center',
+          background: 'white',
+          backgroundSize: '100% 100%',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            color: 'black',
+            fontSize: 62,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          Your answer has been recieved by the great doiq himself.
+        </div>
+        <div
+          style={{
+            color: 'black',
+            fontSize: 40,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          You can doiq again in about X minutes
+        </div>
+      </div>
+    ),
+    intents: [
+      <Button.Reset>Home</Button.Reset>,
+      <Button.Link href='https://docs.doiq.xyz/'>Leaderboard</Button.Link>,
+    ],
+  })
+})
+
+devtools(app, { serveStatic })
+
+export const GET = handle(app)
+export const POST = handle(app)
